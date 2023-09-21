1f95c2fabde56329ff8815cf2c43a76994fb7f85
Tanishq Dhote <tanishqbakka1@gmail.com>
2023-09-22 00:29:57 +0530

    customization

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import './App.css'
 import {useState,useRef} from 'react'
 function App() {
+
   const [theme,setTheme]=useState('white');
   const [text,setText]=useState('');
   const [lineCount,setLineCount]=useState(0);
-  const [numberArray,setNumberArray]=useState([0,1,2]);
+  const [numberArray,setNumberArray]=useState([]);
   const divRef = useRef(null);
   const textareaRef = useRef(null);
+  const fileInputRef = useRef(null);
+  
+ 
+
+  const handleFileOpen = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const fileContent = e.target.result;
+        textareaRef.current.value = fileContent;
+      };
+
+      reader.readAsText(file);
+    }
+  };
+
 
   const handleDivScroll = () => {
     if (divRef.current && textareaRef.current) {
@@ -26,7 +45,7 @@ function App() {
     const blobUrl = window.URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = blobUrl;
-    a.download = 'variable.txt'; 
+    a.download = document.getElementById('file_name').value; 
     a.style.display = 'none';
     document.body.appendChild(a);
     a.click();
@@ -37,6 +56,7 @@ function App() {
     window.document.body.style.backgroundColor=(theme==='white')?'#1b1b1b':'snow'
     setTheme(theme==="white"?'dark':'white')
   }
+
   const handleTextChange=(e)=>{
     const newText=e.target.value;
     setText(newText)
@@ -51,10 +71,25 @@ function App() {
     <>
 
         <div style={{position:'fixed',width:'100%',height:'100%',display:'flex'}}>
-            <div style={{width:'16%',height:'100vh',backgroundColor:'gray',display:'flex',flexDirection:'column'}}> 
-                <button onClick={()=>{changeTheme();alert(numberArray)}}>Theme</button>  
-                <button onClick={handleDownload}> Save </button>
+            <div style={{width:'16%',height:'100vh',backgroundColor:'gray'}}> 
+                
+              <div style={{display:'flex',flexDirection:'column'}}>
+                
+                <div style={{width:'100%'}}>
+                  <p style={{margin:'0rem',fontFamily:'data',fontSize:'1rem',padding:'0rem',paddingTop:'6px',paddingLeft:'10px'}}>File Name</p>
+                  <input style={{margin:'0rem',padding:'0.1rem',fontFamily:'data',fontSize:'1rem',width:'100%',overflowX:'auto',paddingLeft:'10px'}} id="file_name" placeholder='newfile.txt'></input>
+                </div>
+                <p style={{width:'100%',textAlign:'center',margin:'0rem',fontFamily:'data',fontSize:'1rem',padding:'0rem',paddingTop:'6px'}}>Upload</p>
+                <div className="uploadIcon" style={{width:'60%',margin:'auto',height:'10vh',borderRadius:'20px'}}>
+                <input type="file" ref={fileInputRef} onChange={handleFileOpen} style={{width:'100%',height:'100%',opacity:'0',border:'none'}}></input>
+                </div>
+                <p style={{width:'100%',textAlign:'center',margin:'0rem',fontFamily:'data',fontSize:'1rem',padding:'0rem',paddingTop:'6px'}}>Download</p> 
+                <div onClick={handleDownload} className="downloadIcon" style={{width:'60%',height:'10vh',borderRadius:'20px',margin:'auto'}}></div>
+                
+                <div className="settingIcon" style={{width:'60%',height:'10vh',borderRadius:'20px',margin:'auto'}}></div>
+                <button onClick={()=>{changeTheme()}}>Theme</button>  
 
+              </div>
             </div>
             <div style={{width:'100%',height:'100vh',backgroundColor:'darkgrey',display:'flex',flexDirection:'column'}}>
                 <div style={{display:'flex',height:'4%',width:'100%',backgroundColor:'snow',justifyContent:'space-evenly'}}></div> 
